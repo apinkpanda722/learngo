cb9fe14a6231c4db57b025e971b3d22eeb6dd0c2
이진혁 <iih907@mediazen.co.kr>
2024-12-06 17:03:12 +0900

    [feature] dictionary & Add method

--- main.go
+++ main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
-	"learngo/accounts"
+	"learngo/mydict"
 )
 
+// accounts
+//func main() {
+//	account := accounts.NewAccount("nico")
+//	account.Deposit(10)
+//	err := account.Withdraw(5)
+//	fmt.Println(account)
+//	account.ChangeOwner("tale")
+//	if err != nil {
+//		fmt.Println(err)
+//	} else {
+//		fmt.Println(account.Balance(), account.Owner())
+//	}
+//}
+
 func main() {
-	account := accounts.NewAccount("nico")
-	fmt.Println(account)
-	//account.Deposit(10)
-	//err := account.Withdraw(5)
-	//account.ChangeOwner("tale")
+	//dictionary := mydict.Dictionary{"first": "First word"}
+	//definition, err := dictionary.Search("first")
 	//if err != nil {
 	//	fmt.Println(err)
 	//} else {
-	//	fmt.Println(account.Balance(), account.Owner())
+	//	fmt.Println(definition)
 	//}
+
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
+	if err != nil {
+		fmt.Println(err)
+	}
+	hello, _ := dictionary.Search("hello")
+	fmt.Println(hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
 }
